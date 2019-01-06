88586c6a52c7fbc1c3b8dd558caaed0ebaee0db6
Спас Зидаров <zidarovspas@gmail.com>
2019-01-06 17:59:40 +0200

    added a list of engines to the home page and a show page for the engines specs

--- src/Controller/HPCalcController.php
+++ src/Controller/HPCalcController.php
@@ -5,6 +5,8 @@
 	use Symfony\Component\Routing\Annotation\Route;
 	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Doctrine\ORM\Tools\Setup;
+	use Doctrine\ORM\EntityManager;
     
 	use App\Entity\EngineStock;
 
@@ -12,40 +14,51 @@
 	class HPCalcController extends Controller{
 
 	    /**
-	     * @Route("/")
+	     * @Route("/", name="engine_list_home")
 	     */
 		public function index(){
-			//return new Response('<html><body>Hello!</body></html>');
+			$engines= $this->getDoctrine()->getRepository(EngineStock::class)->findAll();
 			
-			return $this->render('pages/index.html.twig');
+			if($engines){
+    			foreach ( $engines as $engine){
+    			    //add_default_engines($engine);
+    			    echo "engine with id:".$engine->getId();
+    			}
+			}else{
+			    //add_default_engines(array(""));
+			}
+			
+			return $this->render('pages/index.html.twig', array('engines' => $engines));
 		}
-
+		
 		/**
-		 * @Route("/engine/add")
+		 * @Route("/engine/{id}", name="engine_show")
 		 */
-		public function add(){
-		    $entityManager = $this->getDoctrine()->getManager();
+		public function show($id){
+		    $engine= $this->getDoctrine()->getRepository(EngineStock::class)->find($id);
 		    
-		    $engine = new EngineStock();
-		    $engine->setManufacturer("Toyota");
-		    $engine->setName("2JZ");
-		    $engine->setProduction("1991-2007");
-		    $engine->setBlockAlloy("Cast Iron");
-		    $engine->setConfiguration("Straight-6");
-		    $engine->setVavlvetrain("DOHC - 4 valves per cylinder");
-		    $engine->setDisplacement("2997cc(3.0L)");
-		    $engine->setHp(325);
-		    $engine->setMaxHpAtRpm(5600);
-		    $engine->setTorque(440);
-		    $engine->setMaxTorqueAtRpm(4800);
-		    $engine->setRedline(6800);
-		    $engine->setLifespan(400000);
-		    $engine->setMaxHpStock(750);
+		    return $this->render('pages/show-engine.html.twig', array('engine' => $engine));
+		}
+		
+		private function add_default_engines($engine){
 		    
-		    $entityManager->persist($engine);
 		    
-		    $entityManager->flush();
+		    $twojz = new EngineStock();
+		    $twojz->setManufacturer("Toyota");
+		    $twojz->setName("2jz");
+		    $twojz->setProduction("1991-2007");
+		    $twojz->setBlockAlloy("Cast Iron");
+		    $twojz->setConfiguration("Straight-6");
+		    $twojz->setVavlvetrain("DOHC - 4 valves per cylinder");
+		    $twojz->setDisplacement("2997cc(3.0L)");
+		    $twojz->setHp(325);
+		    $twojz->setMaxHpAtRpm(5600);
+		    $twojz->setTorque(440);
+		    $twojz->setMaxTorqueAtRpm(4800);
+		    $twojz->setRedline(6800);
+		    $twojz->setLifespan(400000);
+		    $twojz->setMaxHpStock(750);
 		    
-		    return new Response("New engine added with id:".$engine->getId());
+		    return new Response("New engine added with id:".$twojz->getId());
 		}
 	}
